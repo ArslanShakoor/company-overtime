79514ba7212e3751ea22ff5d29a12578dfe2cff5
Arslan Shakoor <arslantechielive@gmail.com>
2017-07-13 00:16:51 -0400

    refacting the post controller and rspec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
   def index
   
   end	
@@ -8,13 +9,24 @@ class PostsController < ApplicationController
   end	
 
   def create
-    @post = Post.new(params.require(:post).permit(:date, :rationale))
-    @post.save
-
-    redirect_to  @post
+    @post = Post.new(require_params)
+    if @post.save
+      redirect_to  @post, notice: "your post was created successfully"
+    else
+      render "new"
+    end    
   end	
 
   def show
-    @post = Post.find(params[:id])
-  end 	
+     
+  end
+
+  private
+    def require_params
+      params.require(:post).permit(:date, :rationale) 
+    end 
+
+    def set_post
+      @post = Post.find(params[:id])
+    end   	
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,26 +12,26 @@ describe 'navigate' do
     visit posts_path
     expect(page).to have_content(/Posts/)
   end	
-  
-  it " can be reached to new_post" do
-    visit new_post_path
+  describe "new_post" do
+    before do 
+      visit new_post_path
+    end  
+
+    it " can be reached to new_post" do
     expect(page.status_code).to eq(200)
-  end
-   
-  it "new_post has content" do
-    visit new_post_path
+    end
+     
+    it "new_post has content" do
     expect(page).to have_content(/New/)
-  end 
-  
-  it 'allows  to create a new post from the new_page' do
-    visit new_post_path
+    end 
+    
+    it 'allows  to create a new post from the new_page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "rationale"
 
-    fill_in 'post[date]', with: Date.today
-    fill_in 'post[rationale]', with: "rationale"
+      click_on "Save"
 
-    click_on "Save"
-
-    expect(page).to have_content("rationale")
+      expect(page).to have_content("rationale")
+    end
   end
-
 end	
