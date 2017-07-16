fdadb0766923c1a71d60ce39c4c02cd286c9af4a
Arslan Shakoor <arslantechielive@gmail.com>
2017-07-15 20:44:24 -0400

    admin user authentication added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  def authenticate_user
+    if current_user == nil
+      redirect_to(root_path)
+    end  
+  end	
 end
---
+++ spec/features/admin_dashboard_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe "admin dashboard" do 
+  
+  it 'cannot be reached when no logged in' do
+     
+
+    visit admin_root_path
+
+    expect(current_path).to eq(user_session_path)
+  end 
+
+  it 'cannot be reached when regular logged in' do
+     user = FactoryGirl.create(:user)
+     login_as(user, :scope => :user)
+
+     visit admin_root_path
+
+     expect(current_path).to eq(root_path)
+  end 
+
+
+  it 'can be reached with admin loggedin' do
+     user = FactoryGirl.create(:admin_user) 
+     login_as(user, :scope => :user)
+
+     visit admin_root_path
+
+     expect(current_path).to eq(admin_root_path)
+  end
+ 
+end	
