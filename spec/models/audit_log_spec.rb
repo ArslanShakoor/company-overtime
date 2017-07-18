e4cc42a166b1eb43562433f1506e52ac1084efd8
Arslan Shakoor <arslantechielive@gmail.com>
2017-07-18 16:20:53 -0400

    audit log model specs implemented with factories

--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before do 
+    @audit_log = FactoryGirl.create(:audit_log)
+	end	
+  describe "creation" do
+    it "can properly created" do
+     	expect(@audit_log).to be_valid
+    end	
+
+    it "should always have user_association" do
+      @audit_log.user_id = nil
+      expect(@audit_log).to_not be_valid    
+    end
+    
+    it "should always have start_date" do
+      @audit_log.start_date = nil
+      expect(@audit_log).to_not be_valid
+    end
+
+    it "should always have status" do
+    	@audit_log.status = nil
+    	expect(@audit_log).to_not be_valid
+
+    end
+
+    it "shoudl have start date 6 weeks prior" do
+     audit_log_check= AuditLog.create(user_id: User.last.id)
+     expect(audit_log_check.start_date).to eq(Date.today - 6.days)
+    end
+  end	
 end
