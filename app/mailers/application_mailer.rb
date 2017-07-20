1e6b4eacf5393b070562c5ed086c5ffdf537dd51
Arslan Shakoor <arslantechielive@gmail.com>
2017-07-19 23:49:27 -0400

    adding email functionality

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'run4fun125@gmail.com'
   layout 'mailer'
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,7 @@
+class UserMailer < ApplicationMailer
+	def email manager
+		@manager = manager
+		mail(to: manager.email, subject: 'Daily overtime requests')
+		
+	end
+end
