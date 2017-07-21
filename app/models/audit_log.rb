99897c2d02b81cc18188af69e59ba7bae9d927ad
Arslan Shakoor <arslantechielive@gmail.com>
2017-07-20 21:45:02 -0400

    implemented the confirmed date for audit log

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -6,9 +6,14 @@ class AuditLog < ApplicationRecord
   validates_presence_of :start_date, :status 
 
   after_initialize :set_defaults
+  before_update :set_end_date,  if: :approved?
 
   private
     def set_defaults
     	self.start_date ||= Date.today - 6.days
     end	
+
+    def set_end_date
+      self.end_date = Date.today
+    end
 end
