f855eb799da3dfb6fc3cda36a70c6939ccadcf02
Arslan Shakoor <arslantechielive@gmail.com>
2017-07-21 20:14:43 -0400

    changes to alerts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def authenticate_user
