cf1ca01feaecd142c1d8247ba8f436dff708961f
Arslan Shakoor <arslantechielive@gmail.com>
2017-07-25 01:19:32 -0400

    some final changes

--- config/application.rb
+++ config/application.rb
@@ -4,13 +4,15 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
 
 module Overtime
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.autoload_paths << Rails.root.join("lib")
+    config.assets.enabled = true
+    config.assets.version = '1.0'
 
      
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,7 @@
   config.cache_classes = true
 
   config.action_mailer.delivery_method = :sparkpost
+  production.config.assets.digest = true
 
 
   # Eager load code on boot. This eager loads most of Rails and
@@ -32,7 +33,7 @@
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
